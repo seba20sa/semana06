942d5cef4eecad667fd0c2b8a9eea234a8766093
Sebastian Sileoni <seba20sa@gmail.com>
2020-11-07 15:33:18 -0300

    added game over feature, blocks that end the game and a reset game function

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Created just the basic canvas and added an element with no animation.
 Modified the canvas added new variables, modified the functions and added the run function. Now we have an element that moves from left to right.
 ## third commit 6/11:
 Added variables for the directional keys modified and separated the action functions for an easier debugging and added the pause so the game starts paused and initiates once the enter key is pressed.
+## forth commit 11/7 
+Added intersection features, randomizer function so the fruits appears in a random location each time the snake head intersects with it.
+## fifth commit 11/7 
+Added new variables modified some functions to get a 'game over' and 'pause' screens. The game restarts everytime the game is over. 
 
 
 
--- js/ninja-snake-01-01.js
+++ js/ninja-snake-01-01.js
@@ -8,8 +8,12 @@ var canvas = null,
     keyRight = 39,
     keyDown = 40,
     direction = 0,
-    x = 30,
-    y = 60;
+    scoring= 0,
+    block =  new Array(),
+    gameOver = true,
+    player = null;
+    
+    
 // for older browsers
 window.requestAnimationFrame = (function () {
     return window.requestAnimationFrame ||
@@ -23,24 +27,86 @@ window.requestAnimationFrame = (function () {
 document.addEventListener('keydown', function (e) {
     lastPressed = e.which;
     }, false);
+
+// function for the rectangle class and intersection of elements
+function rectangle(x, y, width, height) {
+    this.x = (x == null) ? 0 : x;
+    this.y = (y == null) ? 0 : y;
+    this.width = (width == null) ? 0 : width;
+    this.height = (height == null) ? this.width : height;
+    this.intersects = function (rect) {
+        if (rect == null) {
+            window.console.warn('Missing parameters on function intersects');
+        } else {
+        return (this.x < rect.x + rect.width &&
+            this.x + this.width > rect.x &&
+            this.y < rect.y + rect.height &&
+            this.y + this.height > rect.y);
+        }
+    };
+    this.fill = function (ctx) {
+        if (ctx == null) {
+            window.console.warn('Missing parameters on function fill');
+    }    else {
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    };
+}
+// Randomizer for the fruit coordinates
+function randomizer(n) {
+    return Math.floor(Math.random()*n);
+}  
+
+//reset game, scores, directions and snake head position and first fruit
+function resetGame () {
+    scoring = 0;
+    direction = 1;
+    player.x = 100;
+    player.y = 100;
+    fruit.x  = randomizer(canvas.width/10 -1)*10;
+    fruit.y  = randomizer(canvas.height/10 -1)*10;
+    gameOver = false;
+      
+} 
 // drawing of elements
 function paint(ctx) {
     // re doing the canvas
     ctx.fillStyle = '#ddd';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     //first square 
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(x, y, 10, 10);
-    // console.log (lastPressed);
+    ctx.fillStyle = 'yellow';
+    player.fill(ctx);
+    //first fruit
+    ctx.fillStyle = 'green';
+    fruit.fill(ctx);
+    console.log (lastPressed);
     // Showing pause on screen
+   
+    //show killer blocks
+    ctx.fillStyle = 'red';
+    for (x = 0; x < block.length; x++) {
+        block[x].fill(ctx);
+    }
+    //scoring sum
+    ctx.fillText('Scores: ' + scoring, 5, 15);
     if (pause) {
         ctx.textAlling = 'center';
-        ctx.fillText('Game is Paused', 200, 100);
+        if (gameOver) {
+            ctx.fillText('Game Over', 200, 100);
+        } else {
+            ctx.fillText('pause', 200, 100);
+        }  
     }
 }
 //actions automatic movement starting after pause is false
+
 function act() {
+    var x;
     if (!pause) {
+        //Reset the game once Game Over is true.
+        if (gameOver){
+            resetGame();            
+        }
         // directions
         if (lastPressed == keyUp) {
             direction = 0;
@@ -56,32 +122,49 @@ function act() {
         }
         // movement based on previous keystrokes
         if (direction == 0) {
-            y -= 10;
+            player.y -= 10;
         } 
         if (direction == 1) {
-            x += 10;
+            player.x += 10;
         } 
         if (direction == 2) {
-            y += 10;
+            player.y += 10;
         } 
          if (direction == 3) {
-            x -=10;
+            player.x -=10;
         }
         // out of screen control 
-        if (x > canvas.width) {
-            x = 0;
+        if (player.x > canvas.width) {
+            player.x = 0;
         }
-        if (x < 0) {
-            x = canvas.width;
+        if (player.x < 0) {
+            player.x = canvas.width;
         } 
-        if (y > canvas.height) {
-            y = 0;
+        if (player.y > canvas.height) {
+            player.y = 0;
         }
-        if (y < 0) {
-            y = canvas.height;
+        if (player.y < 0) {
+            player.y = canvas.height;
         }
+        // fruit-snake intersection control
+        if(player.intersects(fruit)) {
+            scoring += 1;
+            fruit.x  = randomizer(canvas.width/10 -1)*10;
+            fruit.y  = randomizer(canvas.height/10 -1)*10;
+        } 
+        //control fruit not touching blocks and add pause if player touches a block
+         for (x = 0; x < block.length; x++) {
+            if (fruit.intersects(block[x])) {
+                fruit.x  = randomizer(canvas.width/10 -1)*10;
+                fruit.y  = randomizer(canvas.height/10 -1)*10;
+             }
+             if (player.intersects(block[x])) {
+                gameOver = true;
+                pause = true;                
+             }
+         }
     }
-    // pause toogle
+    // pause toggle
     if (lastPressed == keyEnter) {
         pause = !pause;
         lastPressed = null;
@@ -99,8 +182,18 @@ function run() {
 // now init calls run
 function init() {
     canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');    
+    ctx = canvas.getContext('2d');
+    //player creation with a new object of the rectangle class
+    player = new rectangle(100, 100, 10, 10);
+    //regular fruit
+    fruit = new rectangle(60, 60, 10, 10);
+    //  killer blocks
+    block.push(new rectangle(120, 70, 10, 10));
+    block.push(new rectangle(370, 70, 10, 10));
+    block.push(new rectangle(120, 120, 10, 10));
+    block.push(new rectangle(370, 120, 10, 10));
     run();
     repaint();
 }
 window.addEventListener('load', init, false);
+
